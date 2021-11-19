f6b452a0534b506404048f1c01c231797df27c02
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-19 13:28:41 +0100

    starting reading words method

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,8 +1,14 @@
 package com.codecool;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public List<String> getWords(String fileName) {
+        return null;
     }
 }
