2f981fc60f8080e99597da24210b063b3509a0c6
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-19 14:52:24 +0100

    implement reading words from file

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,21 +1,39 @@
 package com.codecool;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        System.out.println(getWords("test.txt"));
     }
 
-    public int countVocabulary(List<String> words) {
+    public static int countVocabulary(List<String> words) {
         Set<String> wordSet = new HashSet<>(words);
         return wordSet.size();
     }
-  
-    public List<String> getWords(String fileName) {
-        return null;
+
+    public static List<String> getWords(String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            List<String> list = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                addWords(line, list);
+            }
+            return list;
+        }
+    }
+
+    private static void addWords(String line, List<String> list) {
+        String[] words = line.split(" ");
+        for (String word : words) {
+            list.add(word);
+        }
     }
 }
