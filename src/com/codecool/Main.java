e315278bbac4ec86e3149a2a3138cd006a969bcb
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-19 15:02:14 +0100

    add romeo_and_juliet.txt text and finish program

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -11,7 +11,15 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(getWords("test.txt"));
+        if (args.length == 0) {
+            System.out.println("This program counts the different words a text corpus uses. Only distinct words count.");
+            System.out.println("Please add the name of the file as the first argument!");
+            return;
+        }
+        String fileName = args[0]; //"romeo_and_juliet.txt";
+        List<String> words = getWords(fileName);
+        int vocabularyCount = countVocabulary(words);
+        System.out.println(vocabularyCount + " number of different words were used in " + fileName);
     }
 
     public static int countVocabulary(List<String> words) {
