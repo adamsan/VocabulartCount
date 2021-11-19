2b0b425b54e590b0f61a31a3d14ce52b7d77f834
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-19 13:44:31 +0100

    implement vocabulary counting method

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,8 +1,17 @@
 package com.codecool;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public int countVocabulary(List<String> words) {
+        Set<String> wordSet = new HashSet<>(words);
+        return wordSet.size();
     }
 }
